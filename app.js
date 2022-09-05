d751e3a35d468aa3ecfb1ae8740994337b5d373d
Rgabrielart <70184276+Rgabrielart@users.noreply.github.com>
2022-09-05 18:16:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
-var life = 100;// this is my life bar number
-
-life = life - 10;
-
-life = life - 10;
-
- 
-
-
-var name = "Dev Ed"; // this is the name displayed in the game string 
-
-var x = 2; // global variable
-
-
-let a = 5; // math problem variable
-let y = 2;
-let z = a + y;
-
-
-const F = 9; // variable using const 
+const name = 'Gabe';  
 
 
 
+function logger() {
+    console.log("Party Time");
+    console.log("Party Time");
+    console.log("Party Time");
+    console.log("Party Time");
+    
 
-
-
-
-
-console.log(`${life} ${x} ${a} ${y} ${z} ${F}`);
+}
