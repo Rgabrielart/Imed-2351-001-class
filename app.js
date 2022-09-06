0d8f4e8cacf0a8fb23439d5ce7271d998808a882
Rgabrielart <70184276+Rgabrielart@users.noreply.github.com>
2022-09-05 19:38:21 -0500

    changes

--- app.js
+++ app.js
@@ -1,12 +1,59 @@
-const name = 'Gabe';  
+const name = "Dev Ed";  
+
 
 
 
 function logger() {
-    console.log("Party Time");
-    console.log("Party Time");
-    console.log("Party Time");
-    console.log("Party Time");
-    
+    console.log("neverParty Time");
+    console.log("neverParty Time");
+    console.log("neverParty Time");
+    console.log("neverParty Time");
 
+    }
+
+    logger();
+
+
+    function adder(num1, num2) {
+        console.log(num1+num2);
+
+
+    }
+
+adder(5,10);
+
+
+const youtuber = "Traversy Media";
+
+function toUpper(text) {
+    const upperCased = text.toUpperCase();
+    console.log(upperCased);
 }
+
+toUpper(name);
+toUpper(youtuber);
+
+    let car = Object.create(Object.prototype,
+        {
+          name:{
+            value: 'Fiesta',
+            configurable: true,
+            writable: true,
+            enumerable: false
+          },
+          maker:{
+            value: 'Ford',
+            configurable: true,
+            writable: true,
+            enumerable: true
+          }
+        });
+      console.log(car.name)
+    
+      function Vehicle(name, maker) {
+        this.name = name;
+        this.maker = maker;
+     }
+     
+
+     
