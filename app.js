8f0b4bcc1d0f2e0ca03c9c771674d5023c0dd753
Rgabrielart <70184276+Rgabrielart@users.noreply.github.com>
2022-09-13 17:49:33 -0500

    update new assignment

--- app.js
+++ app.js
@@ -1,70 +1,8 @@
-const name = "Dev Ed";  
+const age = 21;
 
-
-
-
-function logger() {
-    console.log("neverParty Time");
-    console.log("neverParty Time");
-    console.log("neverParty Time");
-    console.log("neverParty Time");
-
-    }
-
-    logger();
-
-
-    function adder(num1, num2, num3) {
-        console.log(num1+num2+num3);
-
-
-    }
-
-adder(5,10,10);
-
-
-const youtuber = "Traversy Media";
-
-function toUpper(text) {
-    const upperCased = text.toUpperCase();
-    console.log(upperCased);
+If (age >21){
+console.log("you are old enough to drink!");
+} else {
+    console.log("you shouldn't be drinking.");
 }
-
-toUpper(name);
-toUpper(youtuber);
-
-let carNames;
-
-function carNames =(firstname){
-alert(firstname + "niceCars");
-
-}
-
-firstname("chevy");
-firstname("toyota");
-firstname("camry");
-
-carNames();
-console.log;
-
-
-var score=0;
-console.log(score);
-score= 5;
-console.log(score);
-var score=10; 
-console.log(score);
-
-let scores =5;
-console.log(scores);
-
-
-function addpoints (num){
-score =score +num
-
-}
-addpoints(5);
-console.log
-
-
 
