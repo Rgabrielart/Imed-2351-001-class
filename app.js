cc8b56261e8601c269cea3b2077d08b286bef510
Rgabrielart <70184276+Rgabrielart@users.noreply.github.com>
2022-09-13 18:10:49 -0500

    update

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const age = 21;
 
-If (age >21){
+If (age >21) {
 console.log("you are old enough to drink!");
 } else {
-    console.log("you shouldn't be drinking.");
+    console.log("you shouldn't be drinking");
 }
+
+var dice1 = 9;
+var dice2 = 5;
+
+if (dice1 === 9 && dice2 ===5){
+    console.log("you got this!");
+} else {
+    console.log("not today");
+}
+
+
+let myAge = 100;
+console.log("I am " + myAge + " years old.");
 
