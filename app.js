572cd43239dad0a5bd63642be31e94733454aaf1
Rgabrielart <70184276+Rgabrielart@users.noreply.github.com>
2022-09-13 20:05:15 -0500

    update

--- app.js
+++ app.js
@@ -19,3 +19,7 @@ if (dice1 === 9 && dice2 ===5){
 let myAge = 100;
 console.log("I am " + myAge + " years old.");
 
+let example1a = "abcdefg";
+let example1b = example1a.indexOf("abc");     
+let example1c = example1a.lastIndexOf("efg");  
+
