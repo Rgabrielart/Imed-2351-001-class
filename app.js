8a579b4c5f4a48d87dbaaf8fe4b25b0a6617f5c9
Rgabrielart <70184276+Rgabrielart@users.noreply.github.com>
2022-09-18 20:07:22 -0500

    commit

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ count = count + 1;
 }
 
 
-const element = document.querySelector('.sure');
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    const element = document.querySelector('.demo');
     element.style.color = 'green';
-;
+});
 
 
 
