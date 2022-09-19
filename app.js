cb7d45f9a7efefb4ad4011222aac86cc4e580358
Rgabrielart <70184276+Rgabrielart@users.noreply.github.com>
2022-09-18 21:15:50 -0500

    commit

--- app.js
+++ app.js
@@ -14,11 +14,20 @@ count = count + 1;
 }
 
 
-const text = document.querySelector(".title");
+const text = document.querySelector(".title");   // practice from video
 const changeColor = document.querySelector("changeColor");
 text.style.backgroundColor = "red";
 
 
+const btn = document.querySelector('#btn')             // loop with color
+btn.addEventListener('click', () => {
+    const h1 = document.querySelector('h1')
+    const colors = ['red', 'yellow', 'brown', 'green', 'blue']
+    for (let i = 0; i < colors.length; i++) {
+        h1.style.color = colors[i]
+
+    }
+})
 
 
 
