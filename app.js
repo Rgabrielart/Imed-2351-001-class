6b186e6733beae9743cf44e85705643344b6992d
Rgabrielart <70184276+Rgabrielart@users.noreply.github.com>
2022-09-26 21:52:38 -0500

    commit

--- app.js
+++ app.js
@@ -10,3 +10,24 @@ var dogs = Array("Bulldog", "Beagle", "Labrador");  // Array
 
 dogs[0] = "Bull Terier"; 
 console.log(dogs[1]);
+
+
+
+
+let person = {                       //  object method 
+  firstName: 'John',
+  lastName: 'Doe',
+  greet: function () {
+      console.log('IS this right?');
+  }
+};
+
+
+const persons = {                // object
+
+  name: "Gabe",
+  age: 40
+
+}
+console.log(persons);
+
