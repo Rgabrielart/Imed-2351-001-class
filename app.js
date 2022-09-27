0f26471f5a1ea79788b0998513b2966b137c353d
Rgabrielart <70184276+Rgabrielart@users.noreply.github.com>
2022-09-26 21:23:32 -0500

    commit

--- app.js
+++ app.js
@@ -1,96 +1,12 @@
-const n = 5;
-for (let i = 1; i <= n; i++) {
-    console.log(i);     // for you loop 
-}
+const months = ['January', 'February', 'March', 'April']; // Array method
 
-
-var count = 3; 
-var i = 5;
-
-while (count < 5) {              // while loop 
-    let text = "";
-console.log("im doing while loop");
-console.log("javascript is hard");
-count = count + 1;
-text+="<br>Number of dogs is " + i;
-i++;
-}
-
-
-
-
-
-const text = document.querySelector(".title");   // practice from video
-const changeColor = document.querySelector("changeColor");
-text.style.backgroundColor = "red";
-
-
-
-
-
-
-
-
-
-
-const btn = document.querySelector('#btn');             // loop with color
-btn.addEventListener('click', () => {
-    const h1 = document.querySelector('h1');
-    const colors = ['red', 'yellow', 'brown', 'green', 'blue']
-    for (let i = 0; i < colors.length; i++) {
-        h1.style.color = colors[i];
-
-    }
+months.forEach(function(month) {
+  console.log(month);
 });
 
 
-
-const userlist = document.querySelectorAll('name-list li'); //user  list color 
-console.log(userlist);
-function myFunction(selectTag) {
-    var listValue = selectTag.options[selectTag.selectedIndex].text;
-    document.getElementById('name-list li').style.listStyleType = listValue;
-  }
-
-console.log;
-
-const list = document.querySelector("name-list li");
-const listItems = document.querySelector("name-list");
+var dogs = Array("Bulldog", "Beagle", "Labrador");  // Array
 
 
-
- 
-
-
-namelist.addeventListener("click",function(){
-const newli = document.createElement("button");
-const buttoncontent = document.createTextNode(listInput.value);
-newbutton.appendChild(licontent);
-
-userlist.appendChild(newbutton);
-
-});
-
-
-const listinput = document.querySelector(".listInput");
-console.log(listinput.value);
-
-
-
-
-document.getElementById(".namelist").style.backgroundColor = 'lightgreen';
-
-
-const strawberry  = document.getElementById('strawberry');
-
-strawberry.addEventListener('click', function onClick() {
-  strawberry.style.backgroundColor = 'salmon';
-  strawberry.style.color = 'white';
-});
-
-
-const chocolate = document.getElementById('chocolate');
-chocolate.addEventListener('click', function onClick() {
-    chocolate.style.backgroundColor = 'salmon';
-    chocolate.style.color = 'brown';
-  });
+dogs[0] = "Bull Terier"; 
+console.log(dogs[1]);
