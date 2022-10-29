c28cd9a8149556bd8878efdac963dc0b8dd2bf8e
Rgabrielart <70184276+Rgabrielart@users.noreply.github.com>
2022-10-28 21:53:11 -0500

    updated

--- project 2/app.js
+++ project 2/app.js
@@ -1,34 +1,34 @@
-let data= [
-    {name: 'Matero' , 
-    age: '30'
-
-},
-{name: 'Matero' , 
-age: '30'
-
-},
-{name: 'Sarah' , 
-    age: '30'
-
-},
-{name: 'John' , 
-    age: '30'
-
-},
-{name: 'Sam' , 
-    age: '30'
-
-},
-{name: 'Joey' , 
-    age: '30'
-
-},
-];
-
-const info = document.querySelector('#info')
-
-let details = data.map(function(item) {
-    return '<div>' + Item.name + ''+ 'is'+ item.age + 'years old' + '</div>'; 
-});
-
-info.innerHTML = details.join('\n'); 
+let data = [
+    {
+      name: 'Mateo',
+      age: '30'
+    },
+    {
+      name: 'Sarah',
+      age: '32'
+    },
+    {
+      name: 'John',
+      age: '20'
+    },
+    {
+      name: 'Tim',
+      age: '27'
+    },
+    {
+      name: 'Sam',
+      age: '22'
+    },
+    {
+      name: 'Joey',
+      age: '34'
+    }
+  ];
+  
+  const info = document.querySelector('#info');
+  
+  let details = data.map(function(item) {
+    return '<div>' + item.name + ' ' + 'is ' + item.age + ' years old' + '</div>';
+  });
+  
+  info.innerHTML = details.join('\n'); 
